f1fff264b99147c5442e511e0962c54312fafb1a
P21-0068 <p21-0068@P21-0068.local>
2022-03-15 10:35:39 +0530

    added modules

--- 1.js
+++ 1.js
@@ -3,6 +3,7 @@ var dt = require('./myFirstModule');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.write("Current Date is :"+ dt.myDateTime());
+    // res.write("Current Date is :"+ dt.myDateTime());
     res.end('Hii From OK');
 }).listen(8080);
+
